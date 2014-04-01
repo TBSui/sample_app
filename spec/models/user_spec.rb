8ebc787c86b5ff6324b84421ca3304aa332bf7b7
Tianbo <tianbosui@gmail.com>
2014-04-01 00:36:57 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe User do
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   before { @user = User.new(name: "Example User", email: "user@example.com",
   							password: "foobar", password_confirmation: "foobar") }
   subject { @user }
@@ -14,6 +22,11 @@ describe User do
 
   it { should be_valid }
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+  
   describe "when name is not present" do
   	before { @user.name = " "}
   	it { should_not be_valid}
